f6ea2ed6313207d3166ce510be4c713e34bea450
Laurens Hill <144052420+Laurens-1056274@users.noreply.github.com>
2026-04-08 15:20:57 +0200

    completed BroCode tutorial about varibales (js)

---
+++ cheatsheet.js
@@ -0,0 +1,28 @@
+// To write something in the console use:
+console.log(`Hello World`);
+
+// To create a alert use:
+window.alert(`This is a alert`);
+
+// To change the text from a specefic HTMl ID use:
+document.getElementById(`myH1`).textContent = `Hello World`;
+
+// You can change the text to a specefied variable by usung for example this
+let price = 10.99;
+document.getElementById(`myP`).textContent = `price = ${price}`
+
+// To declare a variable use:
+let;
+// for example
+let x;
+x = 100;
+// or 
+let y = 100;
+// or
+let age = 99;
+console.log(`you are ${age} years old`);
+
+//Booleans are either:
+true
+// or 
+false
---
+++ static/main.js
@@ -0,0 +1,4 @@
+let price = 10.99;
+
+document.getElementById(`myP`).textContent = `price = ${price}`
+
