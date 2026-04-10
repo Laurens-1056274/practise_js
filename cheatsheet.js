b528fda02fdb104c5697244af083c0e8e9153d82
Laurens Hill <144052420+Laurens-1056274@users.noreply.github.com>
2026-04-10 02:28:01 +0200

    completed brocode tutorial about user input

--- cheatsheet.js
+++ cheatsheet.js
@@ -26,3 +26,51 @@ console.log(`you are ${age} years old`);
 true
 // or 
 false
+
+
+/*
+Arithic operators
+*/
+
+// examples arethic operators
+let students;
+students = 30;
+// addition
+students = students + 1;
+// subtraction
+students = students - 1;
+// multiplication
+students = students * 1;
+// division
+students = students / 1;
+// square
+students = students ** 2;
+// modulus (in this case studenst /2. what remains after that is the number of ExtraStudents)
+let ExtrasStudents = students % 2;
+// Just like in python you can note the arethic operator before the =, like for example
+students **= 2
+// if you want to subtract or add only one:
+students--;
+students++;
+
+
+/* 
+Accept user input
+*/
+
+// There are 2 ways to accept user input
+
+// 1. Easy but non professional way = window prompt
+
+let username_test_1 = window.prompt(`whats your username?`);
+
+console.log(username_test_1);
+
+// 2. Professional way = html textbox
+let username_test_2;
+
+document.getElementById("mySubmit").onclick = function(){
+    username_test_2 = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent = `Hello ${username_test_2}`
+
+}
--- static/main.js
+++ static/main.js
@@ -1,4 +1,16 @@
-let price = 10.99;
+// There are 2 ways to accept user input
 
-document.getElementById(`myP`).textContent = `price = ${price}`
+// 1. Easy but non professional way = window prompt
 
+let username_test_1 = window.prompt(`whats ur un?`);
+
+console.log(username_test_1);
+
+// 2. Professional way = html textbox
+let username_test_2;
+
+document.getElementById("mySubmit").onclick = function(){
+    username_test_2 = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent = `Hello ${username_test_2}`
+
+}
